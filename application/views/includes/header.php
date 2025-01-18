9b3c12cefa5476fa3a99ae44b17bf65b1ee55f4e
BADAL_9754 <badalphotosutdio99@gmail.com>
2025-01-18 16:06:01 +0530

    header part

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -1,110 +1,95 @@
-
 <body class="car_insurance bg-light">
 
-<!-- Body Wrap - Start -->
-<div class="page_wrapper">
+  <!-- Body Wrap - Start -->
+  <div class="page_wrapper">
 
-  <!-- Back To Top - Start -->
-  <div class="backtotop">
-    <a href="#" class="scroll">
-      <i class="fa-solid fa-arrow-up"></i>
-    </a>
-  </div>
-  <!-- Back To Top - End -->
+    <!-- Back To Top - Start -->
+    <div class="backtotop">
+      <a href="#" class="scroll">
+        <i class="fa-solid fa-arrow-up"></i>
+      </a>
+    </div>
+    <!-- Back To Top - End -->
 
-  <!-- Site Header - Start
+    <!-- Site Header - Start
   ================================================== -->
-  <header class="site_header header_layout_3">
-    <div class="xb-header stricky">
-      <div class="container">
-        <div class="row align-items-center">
-          <div class="col-lg-3 col-5">
-            <div class="site_logo">
-              <a class="site_link" href="index-2.html">
-                <img src="assets/img/logo.webp" alt="Site Logo White">
-              </a>
-            </div>
-          </div>
-          <div class="col-lg-6 col-2">
-            <nav class="main_menu navbar navbar-expand-lg">
-              <div class="main_menu_inner collapse navbar-collapse justify-content-lg-center" id="main_menu_dropdown">
-                <ul class="main_menu_list unordered_list justify-content-center">
-                  <li >
-                    <a class="nav-link" href="#" id="home_submenu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Home
-                    </a>
-                    
-                  </li>
-                  <li >
-                    <a class="nav-link" href="#" id="services_submenu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Services
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="services_submenu">
-                      <li><a href="services.html">Our Services</a></li>
-                      <li><a href="service_details.html">Service Details</a></li>
-                    </ul>
-                  </li>
-                  <li >
-                    <a class="nav-link" href="#" id="projects_submenu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Projects
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="projects_submenu">
-                      <li><a href="projects.html">Our Projects</a></li>
-                      <li><a href="project_details.html">Project Details</a></li>
-                    </ul>
-                  </li>
-                  <li >
-                    <a class="nav-link" href="#" id="pages_submenu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Pages
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="pages_submenu">
-                      <li><a href="contact.html">Help Center</a></li>
-                      <li >
-                        <a class="nav-link" href="#" id="blog_submenu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          Blogs
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="blog_submenu">
-                          <li><a href="blog.html">Our Blogs</a></li>
-                          <li><a href="blog_details.html">Blog Details</a></li>
-                        </ul>
-                      </li>
-                      <li >
-                        <a class="nav-link" href="#" id="career_submenu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          Career
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="career_submenu">
-                          <li><a href="career.html">Join Us</a></li>
-                          <li><a href="career_details.html">Job Details</a></li>
-                        </ul>
-                      </li>
-                      <li><a href="error.html">404 Error</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="pricing.html">Pricing</a></li>
-                </ul>
-              </div>
-            </nav>
-          </div>
-          <div class="col-lg-3 col-5">
-            <ul class="btns_group p-0 m-0 unordered_list justify-content-end">
-              <li>
-                <button class="mobile_menu_btn bg-primary text-white" type="button" data-bs-toggle="collapse" data-bs-target="#main_menu_dropdown" aria-expanded="false" aria-label="Toggle navigation">
-                  <i class="far fa-bars"></i>
-                </button>
-              </li>
-              <li>
-                <a class="hotline_btn bg-dark" href="tel:8005550199">
-                  <span class="btn_icon">
-                    <img src="assets/images/icons/icon_calling_7.svg" alt="Icon Calling">
-                  </span>
-                  <span class="btn_label" data-text="Hotline: (800) 555‑0199">Hotline: (800) 555‑0199</span>
+    <header class="site_header header_layout_3">
+      <div class="xb-header stricky">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-3 col-5">
+              <div class="site_logo">
+                <a class="site_link" href="index-2.html">
+                  <img src="assets/img/logo.webp" alt="Site Logo White">
                 </a>
-              </li>
-            </ul>
+              </div>
+            </div>
+            <div class="col-lg-6 col-2">
+              <nav class="main_menu navbar navbar-expand-lg">
+                <div class="main_menu_inner collapse navbar-collapse justify-content-lg-center" id="main_menu_dropdown">
+                  <ul class="main_menu_list unordered_list justify-content-center">
+                    <li>
+                      <a class="nav-link" href="#" id="home_submenu" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Home
+                      </a>
+
+                    </li>
+                    <li>
+                      <a class="nav-link" href="#" id="services_submenu" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        About Us
+                      </a>
+
+                    </li>
+                    <li>
+                      <a class="nav-link" href="#" id="projects_submenu" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Demat Account
+                      </a>
+
+                    </li>
+                    <li>
+                      <a class="nav-link" href="#" id="pages_submenu" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Services
+                      </a>
+
+                    </li>
+                    <li>
+                      <a class="nav-link" href="#" id="career_submenu" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Contact
+                      </a>
+
+                    </li>
+ 
+
+                  </ul>
+                </div>
+              </nav>
+            </div>
+            <div class="col-lg-3 col-5">
+              <ul class="btns_group p-0 m-0 unordered_list justify-content-end">
+                <li>
+                  <button class="mobile_menu_btn bg-primary text-white" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#main_menu_dropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="far fa-bars"></i>
+                  </button>
+                </li>
+                <li>
+                  <a class="hotline_btn bg-dark" href="tel:8005550199">
+                    <span class="btn_icon">
+                      <img src="assets/images/icons/icon_calling_7.svg" alt="Icon Calling">
+                    </span>
+                    <span class="btn_label" data-text="Hotline: (800) 555‑0199">Account Opening</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </header>
-  <!-- Site Header - End
+    </header>
+    <!-- Site Header - End
   ================================================== -->
