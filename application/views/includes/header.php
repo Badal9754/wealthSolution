643733d47bcf7a2e5970f87900572d0419feced7
BADAL_9754 <badalphotosutdio99@gmail.com>
2025-01-18 16:29:40 +0530

    share recovery

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -5,7 +5,7 @@
 
     <!-- Back To Top - Start -->
     <div class="backtotop">
-      <a href="#" class="scroll">
+      <a href="" class="scroll">
         <i class="fa-solid fa-arrow-up"></i>
       </a>
     </div>
@@ -19,7 +19,7 @@
           <div class="row align-items-center">
             <div class="col-lg-3 col-5">
               <div class="site_logo">
-                <a class="site_link" href="index-2.html">
+                <a class="site_link" href="">
                   <img src="assets/img/logo.webp" alt="Site Logo White">
                 </a>
               </div>
@@ -29,35 +29,35 @@
                 <div class="main_menu_inner collapse navbar-collapse justify-content-lg-center" id="main_menu_dropdown">
                   <ul class="main_menu_list unordered_list justify-content-center">
                     <li>
-                      <a class="nav-link" href="#" id="home_submenu" role="button" data-bs-toggle="dropdown"
+                      <a class="nav-link" href="" id="home_submenu" role="button" data-bs-toggle="dropdown"
                         aria-expanded="false">
                         Home
                       </a>
 
                     </li>
                     <li>
-                      <a class="nav-link" href="#" id="services_submenu" role="button" data-bs-toggle="dropdown"
+                      <a class="nav-link" href="" id="services_submenu" role="button" data-bs-toggle="dropdown"
                         aria-expanded="false">
                         About Us
                       </a>
 
                     </li>
                     <li>
-                      <a class="nav-link" href="#" id="projects_submenu" role="button" data-bs-toggle="dropdown"
+                      <a class="nav-link" href="" id="projects_submenu" role="button" data-bs-toggle="dropdown"
                         aria-expanded="false">
                         Demat Account
                       </a>
 
                     </li>
                     <li>
-                      <a class="nav-link" href="#" id="pages_submenu" role="button" data-bs-toggle="dropdown"
+                      <a class="nav-link" href="" id="pages_submenu" role="button" data-bs-toggle="dropdown"
                         aria-expanded="false">
                         Services
                       </a>
 
                     </li>
                     <li>
-                      <a class="nav-link" href="#" id="career_submenu" role="button" data-bs-toggle="dropdown"
+                      <a class="nav-link" href="" id="career_submenu" role="button" data-bs-toggle="dropdown"
                         aria-expanded="false">
                         Contact
                       </a>
@@ -73,16 +73,16 @@
               <ul class="btns_group p-0 m-0 unordered_list justify-content-end">
                 <li>
                   <button class="mobile_menu_btn bg-primary text-white" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#main_menu_dropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    data-bs-target="main_menu_dropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <i class="far fa-bars"></i>
                   </button>
                 </li>
                 <li>
-                  <a class="hotline_btn bg-dark" href="tel:8005550199">
+                  <a class="hotline_btn bg-dark" href="">
                     <span class="btn_icon">
                       <img src="assets/images/icons/icon_calling_7.svg" alt="Icon Calling">
                     </span>
-                    <span class="btn_label" data-text="Hotline: (800) 555‑0199">Account Opening</span>
+                    <span class="btn_label">Account Opening</span>
                   </a>
                 </li>
               </ul>
